721193ebdb6e146cc7496b4244413dc1fead004f
VincenzoTavassi <info@egadivacanze.it>
2023-02-24 15:10:17 +0100

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script>
 import AppTitle from "./components/AppTitle.vue";
+import Header from "./components/Header.vue";
 
 export default {
   components: {
     AppTitle,
+    Header,
   },
 };
 </script>
 
 <template>
+  <Header />
   <AppTitle />
 </template>
 
